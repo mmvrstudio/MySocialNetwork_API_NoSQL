6b17b0090ffc65e2c13bd6048502c2657ef350a3
Mariana Marques <65464431+mmvrstudio@users.noreply.github.com>
2022-04-19 19:24:59 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# MySocialNetwork_API_NoSQL
+
+## Description:
+This project is essencially an API created for a social network using the MongoDB database. With this database my website is able to handle large amounts of data. In this project I will use Mongoose and Express.js for routing.
+
+## Installation Guide: 
+1. Download the repository/zip folder 'MySocialNetwork_API_NoSQL'
+2. Install MongoDB
+3. Run the following commands in the integrated terminal (Visual Studio Code)
+    - npm init -y
+    - npm install express
+    - npm install mongoose
+    - npm install moment
+4. Start the server
+    - $ npm start
+5. Install Insomnia
+6. Open Insomnia Core to test API routes
+7. Create New requests: GET, CREATE, DELETE, POST, to visualize and manipulate the data pulled from the database.
+
+ 
+
+## Contact:
+ Github repo: https://github.com/mmvrstudio/MySocialNetwork_API_NoSQL
+
+
---
+++ controllers/thought-controller.js
@@ -0,0 +1,126 @@
+const { User, Thought} = require('../models');
+const thoughtController = {
+  getAllThought(req, res) {
+    Thought.find({})
+      .populate({
+        path: 'reactions',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .populate({
+        path: 'reactions',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'Unable to find thoughts with these ID credentials. Please try to enter a different ID.' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  createThought({ body }, res) {
+    Thought.create(body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { _id: body.userId },
+                { $push: { thoughts: body.thoughtText } },
+                { new: true }
+            );
+        })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'ID Not Found. Unable to find user with this ID. Please enter a different ID.' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.json(err));
+},
+
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'Unable to find thoughts with these ID credentials. Please enter a different ID.' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'Unable to find thoughts with these ID credentials. Please enter a different ID.' });
+          return;
+        }
+        return User.findOneAndUpdate(
+          { _id: parmas.userId },
+          { $pull: { thoughts: params.Id } },
+          { new: true }
+        )
+      })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'Unable to find thoughts with these ID credentials. Please enter a different ID.' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  createReaction({params, body}, res) {
+    Thought.findOneAndUpdate(
+      {_id: params.thoughtId}, 
+      {$push: {reactions: body}}, 
+      {new: true, runValidators: true})
+    .populate({path: 'reactions', select: '-__v'})
+    .select('-__v')
+    .then(dbThoughtData => {
+        if (!dbThoughtData) {
+            res.status(404).json({message: 'No thoughts with this ID.'});
+            return;
+        }
+        res.json(dbThoughtData);
+    })
+    .catch(err => res.status(400).json(err))
+},
+
+  deleteReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'Ops! Try again.'});
+          return;
+        }
+       res.json(dbThoughtData);
+      })
+      .catch(err => res.json(err));
+  }
+};
+module.exports = thoughtController
